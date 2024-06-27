7521e8df4ce60eaec0e7c79fbb578341304cc93d
rcais <rcais@Aishurc>
2024-06-27 11:02:56 +0530

    i am dhiviya

---
+++ JavaBasic/src/Day1/AdditionProgram.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class AdditionProgram {
+	
+	public static void main(String[]args) {
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.print("TNSIF");
+	}
+
+}
